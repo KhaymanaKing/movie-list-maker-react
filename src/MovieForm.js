91bf76a0635e324356824bbbe6a988448898d829
Khayman King <khaymanak@gmail.com>
2022-05-05 11:52:01 -0700

    movie form file should be completed

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export default function MovieForm() {
+export default function MovieForm(movieInfo) {
   return (
-    <div>MovieForm</div>
+    <div>
+      <form className='movie-form' onSubmit={movieInfo.submitMovie}>
+        <label>
+            Title:
+          <input required value ={movieInfo.movieTitle} onChange={(e) => movieInfo.setMovieTitle(e.target.value)}/>
+        </label>
+        <label>
+            Director:
+          <input required value ={movieInfo.movieDirector} onChange={(e) => movieInfo.setMovieDirector(e.target.value)}/>
+        </label>
+        <label>
+            Year Released:
+          <input required type="number" value={movieInfo.movieNumber} onChange={(e) => movieInfo.setMovieNumber(e.target.value)}/>
+        </label>
+        <label>
+            Select Poster Color
+          <select required value ={movieInfo.movieColor} onChange={(e) => movieInfo.setMovieColor(e.target.value)}>
+            <option value='1'>Green</option>
+            <option value='1'>Blue</option>
+            <option value='1'>Red</option>
+            <option value='1'>Purple</option>
+          </select>
+        </label>
+        <button>Add Movie Poster</button>
+      </form>
+
+    </div>
   );
 }
